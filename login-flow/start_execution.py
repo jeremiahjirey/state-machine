1ffdad3ff5c2fcdb63cacb3d5daf3ec2471e24fa
howlcart <imannuelsmkn1bms@gmail.com>
2025-07-05 14:50:33 +0700

    modify

--- login-flow/start_execution.py
+++ login-flow/start_execution.py
@@ -16,11 +16,6 @@ def lambda_handler(event, context):
 
         return {
             'statusCode': 200,
-            'headers': {
-                'Access-Control-Allow-Origin': '*',  
-                'Access-Control-Allow-Headers': 'Content-Type',
-                'Access-Control-Allow-Methods': 'OPTIONS,POST'
-            },
             'body': json.dumps({
                 'message': 'Step Function started!',
                 'executionArn': response['executionArn']
@@ -29,10 +24,5 @@ def lambda_handler(event, context):
     except Exception as e:
         return {
             'statusCode': 500,
-            'headers': {
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Headers': 'Content-Type',
-                'Access-Control-Allow-Methods': 'OPTIONS,POST'
-            },
             'body': json.dumps({ 'error': str(e) })
         }
